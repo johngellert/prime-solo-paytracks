b97d4eb9e987336d478383e46ea33b6a525e3a10
John Gellert <gellertjm@gmail.com>
2019-08-29 21:56:19 -0500

    update login page styles

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,80 +1,91 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './LoginPage.css';
-import { TextField, withStyles, ListItemSecondaryAction } from '@material-ui/core';
-import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./LoginPage.css";
+import {
+  TextField,
+  withStyles,
+  ListItemSecondaryAction
+} from "@material-ui/core";
+import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button";
 
-import smallLogo from './400dpiLogoCropped.png';
+import smallLogo from "./400dpiLogoCropped.png";
+
+import Grid from "@material-ui/core/Grid";
 
 const styles = theme => ({
-  // container: {
-  //     display: 'flex',
-  //     flexWrap: 'wrap',
-  // },
   textField: {
-    marginLeft: theme.spacing(),
-    marginRight: theme.spacing(),
-    marginBottom: theme.spacing(2),
-    width: 300,
+    // marginLeft: theme.spacing(6),
+    // marginRight: theme.spacing(),
+    // marginBottom: theme.spacing(2),
+    // width: "fill"
   },
-  loginButton: {
-    marginLeft: theme.spacing(),
-    marginRight: theme.spacing(),
-    marginBottom: theme.spacing(2),
-    width: 300,
+  button: {
+    // marginLeft: theme.spacing(),
+    // marginRight: theme.spacing(),
+    // marginBottom: theme.spacing(2),
+    // minWidth: 300
   },
   logo: {
     height: 80,
     marginTop: 10,
-    marginBottom: 20,
+    marginBottom: 20
   }
-})
+});
 
 class LoginPage extends Component {
   state = {
-    username: '',
-    password: '',
+    username: "",
+    password: ""
   };
 
-  login = (event) => {
+  login = event => {
     event.preventDefault();
 
     if (this.state.username && this.state.password) {
       this.props.dispatch({
-        type: 'LOGIN',
+        type: "LOGIN",
         payload: {
           username: this.state.username,
-          password: this.state.password,
-        },
+          password: this.state.password
+        }
       });
     } else {
-      this.props.dispatch({ type: 'LOGIN_INPUT_ERROR' });
+      this.props.dispatch({ type: "LOGIN_INPUT_ERROR" });
     }
-  } // end login
+  }; // end login
 
-  handleInputChangeFor = propertyName => (event) => {
+  handleInputChangeFor = propertyName => event => {
     this.setState({
-      [propertyName]: event.target.value,
+      [propertyName]: event.target.value
     });
-  }
+  };
 
   render() {
     const { classes } = this.props;
+
     return (
-      <div id="login-main-container">
+      // <div id="login-main-container">
+      <Grid container spacing={2} direction="column" justify="center" alignItems="center" className="login-main-container">
         {this.props.errors.loginMessage && (
-          <h2
-            className="alert"
-            role="alert"
-          >
+          <h2 className="alert" role="alert">
             {this.props.errors.loginMessage}
           </h2>
         )}
-        <form onSubmit={this.login} id="login-form">
-          <img className={classes.logo} src={smallLogo}/>
-          <h3>Login</h3>
-          <div>
+        <Grid
+          className="login-form"
+          container
+          item
+          spacing={2}
+          direction="column"
+          justify="center"
+          alignItems="center"
+
+        >
+          <Grid item>
+            <img className={classes.logo} src={smallLogo} />
+          </Grid>
+          <Grid item>
             <TextField
               className={classes.textField}
               type="text"
@@ -82,11 +93,10 @@ class LoginPage extends Component {
               label="username"
               variant="outlined"
               value={this.state.username}
-              onChange={this.handleInputChangeFor('username')}
-            >
-            </TextField>
-          </div>
-          <div>
+              onChange={this.handleInputChangeFor("username")}
+            ></TextField>
+          </Grid>
+          <Grid item>
             <TextField
               className={classes.textField}
               type="password"
@@ -94,15 +104,14 @@ class LoginPage extends Component {
               label="password"
               variant="outlined"
               value={this.state.password}
-              onChange={this.handleInputChangeFor('password')}
-            >
-            </TextField>
-          </div>
-          <div>
+              onChange={this.handleInputChangeFor("password")}
+            ></TextField>
+          </Grid>
+
+          <Grid item>
             <Button
-              // className="log-in"
-              
-              className={classes.loginButton}
+              className={classes.button}
+              onClick={this.login}
               type="submit"
               name="submit"
               value="Log In"
@@ -110,40 +119,42 @@ class LoginPage extends Component {
               variant="contained"
             >
               Login
-            </Button >
-          </div>
-          <center  white-space= "pre">
-            <button
-              type="button"
-              className="link-button"
-              onClick={() => { this.props.dispatch({ type: 'SET_TO_REGISTER_MODE' }) }}
+            </Button>
+          </Grid>
+
+          <Grid item>
+            <Button
+              onClick={() => {
+                this.props.dispatch({ type: "SET_TO_REGISTER_MODE" });
+              }}
+              className={classes.button}
+              variant="outlined"
             >
               Register
-            </button>
-            <br />
-            {/* <button
-              type="button"
-              className="link-button"
-            >
-              Forgot password
-            </button> */}
-        </center>
-        </form>
-        
-      </div>
+            </Button>
+          </Grid>
+
+          {/* <button
+            type="button"
+            className="link-button"
+          >
+            Forgot password
+          </button> */}
+        </Grid>
+      </Grid>
     );
   }
 }
 
 LoginPage.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 // Instead of taking everything from state, we just want the error messages.
 // if you wanted you could write this code like this:
 // const mapStateToProps = ({errors}) => ({ errors });
 const mapStateToProps = state => ({
-  errors: state.errors,
+  errors: state.errors
 });
 
 export default connect(mapStateToProps)(withStyles(styles)(LoginPage));
